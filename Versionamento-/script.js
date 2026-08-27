56eddc587b170f55f6f9a74b50f2bbd5a09ac45a
linkdomeuvideo5@gmail.com <gabrielsouzzza16@gmail.com>
2026-08-27 08:45:01 -0300

    Exercicio sei lá o que

--- Versionamento-/script.js
+++ Versionamento-/script.js
@@ -1,17 +1,3 @@
-function alcular_Media() {
-
-    // Variaveis das notas
-    let nota1 = Number (document.getElementById("nota1").value)
-    let nota2 = Number (document.getElementById("nota1").value)
-    let nota3 = Number (document.getElementById("nota1").value)
-
-    // Resultado após calcular a média de todas as notas 
-    let resultado = document.getElementById("resultado")
-
-    let multiplicando = nota1 + nota2 + nota3 
-    let media = multiplicando / 3 
-
-    resultado.textContent = "Sua média é " + media 
-
-    console.log("media")
-}
+    for (let k = 0; k <= 10; k++) {
+        console.log(k)
+    }
