e87cf889f146752e8ef2f2d4d631aa2f42fe1488
linkdomeuvideo5@gmail.com <gabrielsouzzza16@gmail.com>
2026-08-27 08:31:00 -0300

    Exercicio....3?

--- Versionamento-/script.js
+++ Versionamento-/script.js
@@ -1,18 +1,17 @@
-function Maior() {
-    let primeiro = Number (document.getElementById("primeiro").value)
-    let segundo = Number (document.getElementById("segundo").value)
-    let maior = document.getElementById("resultado")
+function alcular_Media() {
 
-    if (primeiro > segundo) {
-        maior.textContent = "O primeiro número é maior"
-    }
- 
-    else if (primeiro < segundo)
-        maior.textContent = "O segundo número é maior "
+    // Variaveis das notas
+    let nota1 = Number (document.getElementById("nota1").value)
+    let nota2 = Number (document.getElementById("nota1").value)
+    let nota3 = Number (document.getElementById("nota1").value)
 
-    else if (primeiro == segundo) {
-        maior.textContent = "Empate, ambos inquais"
-    }
+    // Resultado após calcular a média de todas as notas 
+    let resultado = document.getElementById("resultado")
 
-    
+    let multiplicando = nota1 + nota2 + nota3 
+    let media = multiplicando / 3 
+
+    resultado.textContent = "Sua média é " + media 
+
+    console.log("media")
 }
