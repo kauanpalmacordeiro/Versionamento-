7147064ecfe12f654c48965f8024a53617a4b843
linkdomeuvideo5@gmail.com <gabrielsouzzza16@gmail.com>
2026-08-28 08:52:21 -0300

    enviando codigo incompleto

--- Versionamento-/script.js
+++ Versionamento-/script.js
@@ -1,3 +1,11 @@
-    for (let k = 0; k <= 10; k++) {
-        console.log(k)
+function tabuada() {
+    let numero = Number(document.getElementById("numero").value)
+    let resultado = document.getElementById("resultado")
+
+    for (i == 0; i < 10; i ++) {
+        let tabuada = numero * i
+        resultado.textContent = tabuada
     }
+
+}
+
